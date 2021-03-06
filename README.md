e406395c0a652d4fee0d6534e14c176f7870f693
v1rtl <pilll.pl22@gmail.com>
2021-03-06 16:39:06 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # forwarded
-Deno port of `forwarded` library
+
+![GitHub release (latest by date)][releases] ![https://doc.deno.land/https/deno.land/x/forwarded@v0.8.0/mod.ts][docs]
+
+Deno port of [forwarded](https://github.com/jshttp/forwarded/) library.
+
+## Usage
+
+```ts
+import { serve } from 'https://deno.land/std@0.88.0/http/server.ts'
+import { forwarded, parse } from 'https://deno.land/x/deno-libs/forwarded/mod.ts'
+
+const s = await serve({ port: 3000 })
+
+for await (const req of s) {
+  console.log(forwarded(s))
+}
+```
+
+## API
+
+### `forwarded(req)`
+
+Get all addresses in the request, using the `X-Forwarded-For` header.
+
+### `parse(header)`
+
+Parse the X-Forwarded-For header.
+
+[license]: https://github.com/deno-libs/forwarded/blob/master/LICENSE
+[releases]: https://img.shields.io/github/v/release/deno-libs/forwarded?style=flat-square
+[docs]: https://img.shields.io/github/v/release/deno-libs/forwarded?color=yellow&label=Documentation&logo=deno&style=flat-square
---
+++ mod.ts
@@ -0,0 +1,51 @@
+import { ServerRequest } from 'https://deno.land/std@0.88.0/http/server.ts'
+
+/**
+ * Get all addresses in the request, using the `X-Forwarded-For` header.
+ *
+ * @param req Request object
+ */
+export function forwarded(req: ServerRequest) {
+  // simple header parsing
+  const proxyAddrs = parse(req.headers.get('x-forwarded-for') ?? '')
+  const { hostname: socketAddr } = req.conn.remoteAddr as Deno.NetAddr
+
+  // return all addresses
+  return [socketAddr].concat(proxyAddrs)
+}
+
+/**
+ * Parse the `X-Forwarded-For` header.
+ *
+ * @param header Header value
+ */
+export function parse(header: string) {
+  const list = []
+  let start = header.length
+  let end = header.length
+
+  // gather addresses, backwards
+  for (let i = header.length - 1; i >= 0; i--) {
+    switch (header.charCodeAt(i)) {
+      case 0x20 /*   */:
+        if (start === end) start = end = i
+
+        break
+      case 0x2c /* , */:
+        if (start !== end) list.push(header.substring(start, end))
+
+        start = end = i
+
+        break
+      default:
+        start = i
+
+        break
+    }
+  }
+
+  // final address
+  if (start !== end) list.push(header.substring(start, end))
+
+  return list
+}
